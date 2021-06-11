13d39937754f727700605d36fa3fb3b069b40cd1
KimHaNeul <s2025@e-mirim.hs.kr>
2021-06-12 00:06:33 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/lotto/MainActivity.kt
@@ -0,0 +1,39 @@
+package org.techtown.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val lotteryNumbers = arrayListOf(number1,number2,number3,number4,number5,number6)
+        val countDownTimer = object:CountDownTimer(3000,100){
+            override fun onTick(p0: Long) {
+                lotteryNumbers.forEach{
+                    val randomNumber = (Math.random()*45 + 1).toInt() //1~45의 랜덤값
+                    it.text = "$randomNumber"
+                }
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+
+        lotteryButton.setOnClickListener{
+            if(lotteryButton.isAnimating){
+             lotteryButton.cancelAnimation()
+             countDownTimer.cancel()
+            }
+            else {
+                lotteryButton.playAnimation()
+                countDownTimer.start()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/techtown/lotto/SplashActivity.kt
@@ -0,0 +1,35 @@
+package org.techtown.lotto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = Runnable {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        handler.postDelayed(runnable,3000)
+
+        animationView.setOnClickListener{
+            handler.removeCallbacks(runnable)
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+            //main액티비티로 갔다가 다시 돌아오는 것 방지
+            finish()
+        }
+    }
+
+}
